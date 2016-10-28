9fc3eb7e32217eeb5921aa04ad6cc61aaaaa2b66
Francesco <francesco.colombo@gmail.com>
2016-10-28 11:02:45 +0200

    add iOS cell template

---
+++ ToDoList/ToDoList.iOS/Resources/Layouts/Cells/TaskListViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace ToDoList.iOS
+{
+    public partial class TaskListViewCell : UITableViewCell
+    {
+        public static readonly NSString Key = new NSString ("TaskListViewCell");
+        public static readonly UINib Nib;
+
+        static TaskListViewCell ()
+        {
+            Nib = UINib.FromName ("TaskListViewCell", NSBundle.MainBundle);
+        }
+
+        protected TaskListViewCell (IntPtr handle) : base (handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
